a699eb58a4d138e272ecffa69223ff56e3cd572e
docbull <fudghks21@gmail.com>
2022-05-27 20:59:44 +0900

    version updated

--- hardhat-nft/contracts/MyNFT.sol
+++ hardhat-nft/contracts/MyNFT.sol
@@ -1,11 +1,11 @@
 //Contract based on https://docs.openzeppelin.com/contracts/3.x/erc721
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.7.3;
+pragma solidity ^0.8.14;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract MyNFT is ERC721, Ownable {
     using Counters for Counters.Counter;
